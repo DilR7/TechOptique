67f8f6b042f861077a67b9edd02221a2cf33561a
DilR7 <erictianto12@gmail.com>
2024-04-30 15:08:44 +0700

    Make User Front End

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\SubCategoryController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +20,15 @@ use App\Http\Controllers\Admin\SubCategoryController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(HomeController::class)->group(function (){
+    Route::get('/', 'index')->name('Home');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', 'role:user'])->name('dashboard');
 
-Route::middleware(['auth', 'role:user'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::controller(DashboardController::class)->group(function(){
         Route::get('/admin/dashboard', 'index')->name('admindashboard');
     });
