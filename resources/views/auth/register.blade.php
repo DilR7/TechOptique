2aa892daa8dd7da03420c7255ac1df09624cdf13
Eric Tianto <eric.tianto@binus.edu>
2024-05-03 14:08:03 +0700

    Make Some Change in Login and Register UI Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -206,7 +206,7 @@
 
                     <div>
                         <button type="submit"
-                            class="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">{{ __('Register') }}</button>
+                            class="mt-3 w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">{{ __('Register') }}</button>
                     </div>
                 </form>
                 <div class="mt-4 text-sm text-gray-600 text-center">
